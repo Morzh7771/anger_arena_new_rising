a76e280315c0cb10dbcde881774b6302e9fa7ac5
Morzh <imhoserge@gmail.com>
2023-06-16 22:50:40 +0300

    Base addon

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1,7 +1,7 @@
 -- Generated from template
 
-if CAddonTemplateGameMode == nil then
-	CAddonTemplateGameMode = class({})
+if AngelArena == nil then
+	_G.AngelArena = class({})
 end
 
 function Precache( context )
@@ -13,20 +13,51 @@ function Precache( context )
 			PrecacheResource( "particle_folder", "particles/folder", context )
 	]]
 end
+local Constants = require('consts')
 
--- Create the game mode when we activate
 function Activate()
-	GameRules.AddonTemplate = CAddonTemplateGameMode()
+	GameRules.AddonTemplate = AngelArena()
 	GameRules.AddonTemplate:InitGameMode()
 end
 
-function CAddonTemplateGameMode:InitGameMode()
-	print( "Template addon is loaded." )
+function AngelArena:InitGameMode()
+	local GameMode = GameRules:GetGameModeEntity()
+	GameMode:SetCustomXPRequiredToReachNextLevel(Constants.XP_PER_LEVEL_TABLE)
+	GameMode:SetUseCustomHeroLevels(true)
+
+	GameMode:SetFountainPercentageHealthRegen(7)
+	GameMode:SetFountainPercentageManaRegen(10)
+	GameMode:SetFountainConstantManaRegen(20)
+
+	GameMode:SetFreeCourierModeEnabled( true )
+	GameMode:SetBuybackEnabled(true)
+	GameMode:SetStashPurchasingDisabled(false)
+	GameMode:SetLoseGoldOnDeath(true)
+
+	GameMode:SetDraftingHeroPickSelectTimeOverride(60)
+	GameMode:SetDraftingBanningTimeOverride(20)
+	GameRules:SetPostGameTime(30)
+	GameRules:SetStrategyTime(15.0)
+	GameRules:SetHeroSelectionTime(90)
+
+	GameMode:SetCustomBackpackSwapCooldown(4.0)
+	GameRules:SetStartingGold(750)
+	
+	GameRules:SetUseUniversalShopMode(true)
+
+	-- AttributeDerivedStats
+	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_STRENGTH_MAGIC_RESISTANCE_PERCENT, 0)
+	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_STRENGTH_STATUS_RESISTANCE_PERCENT, 0)
+	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_STRENGTH_HP_REGEN, 0)
+	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_AGILITY_MOVE_SPEED_PERCENT, 0)
+	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_INTELLIGENCE_MANA_REGEN, 0)
+	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_INTELLIGENCE_MAGIC_RESISTANCE_PERCENT, 0)
+
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
 end
 
 -- Evaluate the state of the game
-function CAddonTemplateGameMode:OnThink()
+function AngelArena:OnThink()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
 		--print( "Template addon script is running." )
 	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
