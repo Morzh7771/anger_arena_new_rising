b87ce6cb08fc8796085dc785073b721480817d2e
Morzh <imhoserge@gmail.com>
2023-06-19 14:41:16 +0300

    duel comback

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -12,21 +12,41 @@ function Precache( context )
 			PrecacheResource( "particle", "*.vpcf", context )
 			PrecacheResource( "particle_folder", "particles/folder", context )
 	]]
+	DuelController:Precache(context)
+	--ItemPrecache:Precache(context)
 end
+require('lib/teleport')
 require('lib/boss/boss_spawner')
 require('lib/timers')
 require('lib/team_helper')
 require('lib/spawners/creep_spawner')
 require('lib/spawners/creep_leveling')
+require('lib/duel/duel_controller')
+require('lib/attentions')
+require('lib/gpm_lib')
+require('lib/game_ender')
+require('lib/move_limiter')
+require('lib/comeback_system')
 local postRequireList = {
 	'lib/base/player',
 	'lib/base/base_npc'
 }
 local armor_table = require('creeps/armor_table_summon') -- armor to units
 local Constants = require('consts')
-local killToWin = 100
 local cheat = false
-local first = true
+local is_game_start = false
+local is_game_end = false
+local game_start_for_courier = false
+
+GOLD_PER_TICK = 4
+
+KILL_LIMIT = 1
+
+GOLD_FOR_COUR = 350
+
+_G.Kills = {}
+_G.KILL_LIMIT  = KILL_LIMIT
+
 RESPAWN_MODIFER = 0.135
 function Activate()
 	GameRules.AddonTemplate = AngelArena()
@@ -39,18 +59,91 @@ function Activate()
     end
 end
 
+function UpdatePlayersCount()
+	if is_game_end then return end
+
+	local teamsData = 
+	{
+		[DOTA_TEAM_GOODGUYS] = 0,
+		[DOTA_TEAM_BADGUYS]  = 0,
+	}
+
+	local nPlayersConnected = 0
+
+	TeamHelper:ApplyForPlayers(nil, function(playerid)
+		if IsAbadonedPlayerID(playerid) then return end
+
+		local team = PlayerResource:GetTeam(playerid)
+
+		-- ignore invalid team
+		if team == 0 then return end
+
+		teamsData[team] = (teamsData[team] or 0) + 1
+		nPlayersConnected = nPlayersConnected + 1
+	end)
+
+	local nRadiants = teamsData[DOTA_TEAM_GOODGUYS]
+	local nDire = teamsData[DOTA_TEAM_BADGUYS]
+
+	if nPlayersConnected == 0 or (nRadiants == 0 or nDire == 0 and not GameRules:IsCheatMode()) then
+		local team
+
+		if nPlayersConnected == 0 then
+			team = DOTA_TEAM_NEUTRALS -- creeps is the best
+		elseif nRadiants == 0 then
+			team = DOTA_TEAM_BADGUYS
+		else
+			team = DOTA_TEAM_GOODGUYS
+		end
+
+		DuelController:SetFreezeTimer( true )
+		GameEnder:StartGameEnd( team )
+		is_game_end = true
+	end
+end
+function BackPlayersToMap()
+	local heroes = HeroList:GetAllHeroes()
+
+	for _, hero in pairs(heroes) do
+		if hero and hero:IsAlive() then
+
+			if not hero._info then
+				function CheckPos(unit, pos)
+					return pos[3] >= -2500
+				end
+
+				function GetBasePosition(team)
+					if team == DOTA_TEAM_GOODGUYS then
+						return Entities:FindByName(nil, "RADIANT_BASE"):GetAbsOrigin()
+					elseif team == DOTA_TEAM_BADGUYS then
+						return Entities:FindByName(nil, "DIRE_BASE"):GetAbsOrigin()
+					end
+				end
+
+				local nSaveSteps = 20
+				local fMinDist = 100
+				hero._info = MakeMoveLimiter(hero, CheckPos, nSaveSteps, fMinDist, GetBasePosition(hero:GetTeamNumber()) )
+			end
+
+			hero._info:tick()
+		end
+	end
+end
 function AngelArena:InitGameMode()
 	for _, moduleName in pairs(postRequireList) do
 		require(moduleName)
 	end
 	local GameMode = GameRules:GetGameModeEntity()
+
+	GameRules:SetCustomVictoryMessage("#aa_on_win_message")
+
+	GameRules:SetPreGameTime(60) -- old 9
 	BossSpawner:Init()
 	CreepSpawner:Init()
 
 	CreepSpawner:RegisterOnSpawnCallback(function(arg) CreepLeveling:OnSpawnCallback(arg); end)
 	CreepSpawner:RegisterOnDeathCallback(function(arg) CreepLeveling:OnDeathCallback(arg); end)
 	if GameRules:IsCheatMode()then
-		killToWin = 10000000
 		cheat = true
 	end
 	GameMode:SetCustomXPRequiredToReachNextLevel(Constants.XP_PER_LEVEL_TABLE)
@@ -77,9 +170,14 @@ function AngelArena:InitGameMode()
 	
 	GameRules:SetUseUniversalShopMode(true)
 
-	ListenToGameEvent( "dota_team_kill_credit", Dynamic_Wrap( AngelArena, 'OnTeamKillCredit' ), self )
+
 	ListenToGameEvent( "npc_spawned", Dynamic_Wrap( AngelArena, "OnNPCSpawned" ), self )
+	ListenToGameEvent('game_rules_state_change', Safe_Wrap(AngelArena, 'OnGameStateChange'), self)
 	ListenToGameEvent("entity_killed", Dynamic_Wrap(AngelArena, "OnEntityKilled"), self)
+
+
+	LinkLuaModifier("modifier_godmode", 'modifiers/modifier_godmode', LUA_MODIFIER_MOTION_NONE)
+	LinkLuaModifier("modifier_full_disable_stun", 'modifiers/modifier_full_disable_stun', LUA_MODIFIER_MOTION_NONE)
 	-- AttributeDerivedStats
 	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_STRENGTH_MAGIC_RESISTANCE_PERCENT, 0)
 	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_STRENGTH_STATUS_RESISTANCE_PERCENT, 0)
@@ -89,37 +187,116 @@ function AngelArena:InitGameMode()
 	--GameMode:SetCustomAttributeDerivedStatValue(DOTA_ATTRIBUTE_INTELLIGENCE_MAGIC_RESISTANCE_PERCENT, 0)
 
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
+
+	GameMode:SetCustomRadiantScore(0)
+	GameMode:SetCustomDireScore(0)
+	_G.Kills[DOTA_TEAM_GOODGUYS] = _G.Kills[DOTA_TEAM_GOODGUYS] or 0
+	_G.Kills[DOTA_TEAM_BADGUYS] = _G.Kills[DOTA_TEAM_BADGUYS] or 0
+
+	AngelArena:OnGameStateChange()
+	Attentions:SetKillLimit( KILL_LIMIT )
+
 end
 
 -- Evaluate the state of the game
 function AngelArena:OnThink()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		if first == true then
-			BossSpawner:OnGameStart()
-			CreepSpawner:StartSpawning()
-			first = false
-			print('boss game start')
-		end
 	end
 	if GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 		return nil
 	end
 	return 1
 end
-function AngelArena:EndGame( victoryTeam )
-	GameRules:SetGameWinner( victoryTeam )
-	GameRules:SetCustomVictoryMessage('Мамут Рахал')
+function AngelArena:ShareGold()
+	local teamGolds = {}
+
+	TeamHelper:ApplyForPlayers(nil, function(playerID)
+		local team = PlayerResource:GetTeam(playerID)
+
+		local teamData = teamGolds[team] or {
+			nPlayers = 0,
+			freeGold = 0,
+		}
+
+		if PlayerResource:IsConnected( playerID ) then
+			teamData.nPlayers = teamData.nPlayers + 1
+		end
+
+		if not PlayerResource:IsAbandoned( playerID ) then return end
+
+		teamData.freeGold = teamData.freeGold + PlayerResource:GetGold( playerID )
+
+		PlayerResource:SetGold(playerID, 0, false)
+		PlayerResource:SetGold(playerID, 0, true)
+	end)
+
+
+	TeamHelper:ApplyForPlayers(nil, function(playerID)
+		local team = PlayerResource:GetTeam(playerID)
+
+		local teamData = teamGolds[team]
+
+		if not teamData then return end
+
+		local newGold = teamData.freeGold / teamData.nPlayers
+
+		PlayerResource:ModifyGold(playerID, newGold, true, 0)
+	end)
 end
+function AngelArena:OnGameStateChange()
+	if GetMapName() ~= "map_5x5_cm" then
+		if GameRules:State_Get() == DOTA_GAMERULES_STATE_TEAM_SHOWCASE then
 
-function AngelArena:OnTeamKillCredit(event)
-	DeepPrintTable(event)
-	if killToWin <= GetTeamHeroKills(DOTA_TEAM_GOODGUYS) then 
-		AngelArena:EndGame( DOTA_TEAM_GOODGUYS )
-	else
+			TeamHelper:ApplyForPlayers(team, function(ply_id)
+				local player = PlayerResource:GetPlayer(ply_id)
 
+				if player and PlayerResource:IsValidPlayer(ply_id) and PlayerResource:GetSelectedHeroName(ply_id) == "" then
+					player:MakeRandomHeroSelection()
+				end
+			end)
+		end
 	end
-	if killToWin <= GetTeamHeroKills(DOTA_TEAM_BADGUYS) then
-		AngelArena:EndGame( DOTA_TEAM_BADGUYS )
+
+	if GameRules:State_Get() == DOTA_GAMERULES_STATE_PRE_GAME then
+		PauseGame(true)
+		CustomGameEventManager:Send_ServerToAllClients("MakeNeutralItemsInShopColored", {})
+
+		
+		local spawners = Entities:FindAllByClassname("npc_dota_neutral_spawner")
+
+		for _, spawner in pairs(spawners) do
+			UTIL_Remove(spawner)
+		end
+	end
+
+	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		if not is_game_start then
+			CreepSpawner:StartSpawning()
+			BossSpawner:OnGameStart()
+
+			Timers:CreateTimer(0.1, function()
+				GPM_Init()
+			end)
+
+			Timers:CreateTimer(10, function() -- таймер для шаринга голды
+				AngelArena:ShareGold()
+				UpdatePlayersCount()
+				return 10
+			end)
+
+			Timers:CreateTimer(0.5, function()
+				BackPlayersToMap()
+				return 0.5
+			end)
+
+			DuelController:OnGameStart(function(winnerTeam)
+				is_game_end = true
+				DuelController:SetFreezeTimer(true)
+				GameEnder:ForceEnd(winnerTeam)
+			end)
+
+			is_game_start = true
+		end
 	end
 end
 function AngelArena:OnNPCSpawned(keys)
@@ -155,24 +332,85 @@ function AngelArena:OnLevelUp(keys)
 		hero:SetAbilityPoints(hero:GetAbilityPoints() + 1)
 	end
 end
+
 function AngelArena:OnEntityKilled(event)
 	local killedUnit = EntIndexToHScript(event.entindex_killed)
 	local killedTeam = killedUnit:GetTeam()
 	local hero = EntIndexToHScript(event.entindex_attacker)
 	local heroTeam = hero:GetTeam()
 
+	_G.Kills[heroTeam] = _G.Kills[heroTeam] or 0
+	_G.Kills[DOTA_TEAM_BADGUYS] = _G.Kills[DOTA_TEAM_BADGUYS] or 0
+	_G.Kills[DOTA_TEAM_GOODGUYS] = _G.Kills[DOTA_TEAM_GOODGUYS] or 0
+
+	local GameMode = GameRules:GetGameModeEntity()
+
+	if not killedUnit or not IsValidEntity(killedUnit) then return end
+
 	if BossSpawner:HandleUnitKill( killedUnit, hero ) then
 		return
 	end
+
 	if IsValidEntity(killedUnit) and not killedUnit:IsAlive() and killedUnit:IsRealHero() then
 		local timeLeft = killedUnit:GetLevel() * 3.8 + 5
 		timeLeft = timeLeft * RESPAWN_MODIFER
 		if timeLeft < 5.0 then
 			timeLeft = 5.0
 		end
+
 		if killedUnit:IsReincarnating() == false then
-			print(timeLeft)
 			killedUnit:SetTimeUntilRespawn(timeLeft)
 		end
 	end
+
+	if killedUnit:IsRealHero() and not killedUnit:IsReincarnating() and heroTeam and heroTeam ~= killedTeam and _G.Kills[heroTeam] then
+		_G.Kills[heroTeam] = _G.Kills[heroTeam] + 1
+	end
+
+	if (killedUnit:HasAbility("skeleton_king_reincarnation") or killedUnit:HasAbility("angel_arena_reincarnation")) then
+		local rein_ability = killedUnit:FindAbilityByName("skeleton_king_reincarnation") or killedUnit:FindAbilityByName("angel_arena_reincarnation")
+		local ability_level = rein_ability:GetLevel()
+		local current_cooldown = rein_ability:GetCooldownTimeRemaining()
+		local cooldown = Util:GetReallyCooldown(killedUnit, rein_ability)
+
+		if (current_cooldown ~= cooldown) then
+			while (killedUnit:HasModifier("modifier_item_aegis")) do
+				killedUnit:RemoveModifierByName("modifier_item_aegis")
+			end
+		end
+	else
+		while (killedUnit:HasModifier("modifier_item_aegis")) do
+			killedUnit:RemoveModifierByName("modifier_item_aegis")
+		end
+	end
+
+	if killedUnit:IsRealHero() and not killedUnit:IsReincarnating() then
+		self:OnHeroDeath(killedUnit, hero)
+	end
+
+	local allowedTeams = {
+		DOTA_TEAM_GOODGUYS,
+		DOTA_TEAM_BADGUYS,
+	}
+
+	for _, teamNumber in pairs(allowedTeams) do
+		if _G.Kills[teamNumber] >= KILL_LIMIT and not DuelController:IsLastDuelTeam(teamNumber) then
+			DuelController:AddLastDuelTeam( teamNumber )
+			Attentions:SendAttentionText("#aa_last_duel_coming", 10, 2)
+			Attentions:SendChatMessage("#aa_last_duel_coming")
+		end
+	end
+
+	GameMode:SetCustomDireScore( _G.Kills[DOTA_TEAM_BADGUYS] )
+	GameMode:SetCustomRadiantScore( _G.Kills[DOTA_TEAM_GOODGUYS] )
+
+	if hero and hero:GetPlayerOwnerID() ~= nil and killedUnit:IsCourier() then
+		PlayerResource:ModifyGold(hero:GetPlayerOwnerID(), GOLD_FOR_COUR, false, 0)
+	end
+end
+function AngelArena:OnHeroDeath(dead_hero, killer)
+	if not dead_hero or dead_hero:IsNull() or not IsValidEntity(dead_hero) then return end
+	if not killer or killer:IsNull() or not IsValidEntity(killer) then return end
+
+	ComebackSystem:OnKill( killer:GetPlayerOwnerID(), killer:GetTeamNumber(), dead_hero:GetPlayerOwnerID(), dead_hero:GetTeamNumber() )
 end
