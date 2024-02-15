0cad28f78be5231109109ad313e63754bef95bce
Chemze <evg13eny@gmail.com>
2024-02-15 20:09:04 +0500

    Ench

--- scripts/vscripts/lib/percent_damage.lua
+++ scripts/vscripts/lib/percent_damage.lua
@@ -130,7 +130,8 @@ function PercentDamage:_init()
 		"ancient_apparition_ice_vortex",
 		"ancient_apparition_cold_feet",
 		"skywrath_mage_concussive_shot",
-		"skywrath_mage_mystic_flare"
+		"skywrath_mage_mystic_flare",
+		"enchantress_enchant"
 	}
 	local MagicalDamageFromAll = {
 		"mirana_arrow",
