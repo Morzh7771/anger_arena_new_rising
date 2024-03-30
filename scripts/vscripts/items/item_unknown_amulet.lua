ec6f2aa8b26cd9fcfcd762c3f96d360fc34ebef0
Chemze <evg13eny@gmail.com>
2024-03-30 17:49:42 +0500

    amulet damage aren't show, but working

--- scripts/vscripts/items/item_unknown_amulet.lua
+++ scripts/vscripts/items/item_unknown_amulet.lua
@@ -48,12 +48,10 @@ LinkLuaModifier("modifier_item_unknown_amulet_abilites", "items/item_unknown_amu
 item_unknown_amulet = class({
 	GetIntrinsicModifierName = function (self) return "modifier_item_unknown_amulet_stats" end,
 	OnSpellStart = function (self)
-        if not IsServer() then return end
         if self:GetSecondaryCharges() > 3 then
             self:SetSecondaryCharges(1)
         else self:SetSecondaryCharges(self:GetSecondaryCharges() + 1)
         end
-        self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_item_unknown_amulet_abilites",{})
         print(self:GetSecondaryCharges())
     end,
     OnDestroy =  function (self)
@@ -61,65 +59,54 @@ item_unknown_amulet = class({
     end,
 })
 
-modifier_item_unknown_amulet_abilites = class({
-    IsHidden = function() return false end,
-    IsPurgable = function() return false end,
-    DestroyOnExpire = function() return false end,
-    DeclareFunctions = function(self) return {
-       MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE,
-    } end,
-    OnCreated = function (self)
-        if not IsServer() then return end
-        self.ef_hp = 0
-        self.def = 0
-        self:StartIntervalThink(0.3)
-    end,
-    OnIntervalThink = function (self)
-        if self:GetAbility():GetSecondaryCharges() == 1 then
-            self.ef_hp = (self:GetAbility():GetCurrentCharges() * 0.1 * self:GetParent():GetMaxHealth()) + self:GetParent():GetMaxHealth()
-        end
-        self.def = 
-    end,
-    GetModifierIncomingDamage_Percentage = function (self)
-        if self:GetAbility():GetSecondaryCharges() == 1 then
-            return -100
-        else
-            return 0 
-        end
-    end,
-
-    OnDestroy = function (self)
-        self:GetAbility():RemoveAllModifiersByName("modifier_item_unknown_amulet_abilites")
-        print("Destroy")
-    end,
-    
-})
-
 modifier_item_unknown_amulet_stats = class({
 	IsPassive = function() return false end,
     IsHidden = function() return false end,
     IsPurgable = function() return false end,
     DestroyOnExpire = function() return false end,
-    GetAttributes = function(self) return MODIFIER_ATTRIBUTE_MULTIPLE end,
+    GetAttributes = function(self) return MODIFIER_ATTRIBUTE_PERMANENT end,
     DeclareFunctions = function(self) return {
         MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
 		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
 		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+        MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE,
+        MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE,
+        MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
     } end,
-    OnCreated = function (self)
-    	if not IsServer() then return end
+    OnCreated = function (self,kv)
+        self.ef_hp = 0
+        self.def = 0
+        self.hand_damage = 0
+        self.spell_amp = 0
+        self.all_damage_def_per_charge = self:GetAbility():GetSpecialValueFor("all_damage_def_per_charge") / 100
+        self.hand_damage_per_charge = self:GetAbility():GetSpecialValueFor("hand_damage_per_charge")
+        self.spell_amp_pre_charge = self:GetAbility():GetSpecialValueFor("spell_amp_pre_charge")
     	self:StartIntervalThink(0.3)
 	end,
 	OnIntervalThink = function (self)
-		if not IsServer() then return end
 		self.primary_attribute_pct = self:GetAbility():GetSpecialValueFor("charge_primary_attribute") * self:GetAbility():GetCurrentCharges()
         self.mode = self:GetAbility():GetSecondaryCharges()
     	self:GetParent():CalculateStatBonus(true)
+        --Strength Mode
+        if self:GetAbility():GetSecondaryCharges() == 1 then
+            self.ef_hp = (self:GetAbility():GetCurrentCharges() * self.all_damage_def_per_charge * self:GetParent():GetMaxHealth()) + self:GetParent():GetMaxHealth()
+            self.def = ((self:GetParent():GetMaxHealth() / self.ef_hp) - 1) * 100
+            print(self.def)
+        end
+        --Agility Mode
+        if self:GetAbility():GetSecondaryCharges() == 2 then
+            self.hand_damage = self:GetAbility():GetCurrentCharges() *  self.hand_damage_per_charge
+            print(self.hand_damage)
+        end
+        --Intellect Mode
+        if self:GetAbility():GetSecondaryCharges() == 3 then
+            self.spell_amp = self:GetAbility():GetCurrentCharges() * self.spell_amp_pre_charge
+            print(self.spell_amp)
+        end
+        --Univ Mode
+        if self:GetAbility():GetSecondaryCharges() == 4 then
+        end
 	end,
-    OnDestroy = function (self)
-        self:GetAbility():RemoveAllModifiersOfName("modifier_item_unknown_amulet_abilites")
-        print("Destroy__2")
-    end,
 	GetModifierBonusStats_Strength = function (self)
 		if self.mode == 1 then
     	    return self.primary_attribute_pct
@@ -141,4 +128,19 @@ modifier_item_unknown_amulet_stats = class({
     	    return self.primary_attribute_pct / 3 
     	end
 	end,
+    GetModifierIncomingDamage_Percentage = function (self)
+        if self:GetAbility():GetSecondaryCharges() == 1 then
+            return self.def
+        else
+            return 0 
+        end
+    end,  
+    GetModifierBaseDamageOutgoing_Percentage = function (self)
+        if self:GetAbility():GetSecondaryCharges() == 2 then
+            return self.hand_damage end 
+    end,
+    GetModifierSpellAmplify_Percentage = function (self)
+        if self:GetAbility():GetSecondaryCharges() == 3 then
+            return self.spell_amp end
+    end,
 })
