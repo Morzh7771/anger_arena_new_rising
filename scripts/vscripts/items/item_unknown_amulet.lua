c038f858e96e9f90026fcdef1b470795589f9128
Chemze <evg13eny@gmail.com>
2024-04-02 03:20:57 +0500

    unk_am 95%rdy and (mb) fix 10 barotrum

--- scripts/vscripts/items/item_unknown_amulet.lua
+++ scripts/vscripts/items/item_unknown_amulet.lua
@@ -29,14 +29,17 @@ modifier_item_unknown_amulet_stats = class({
     DestroyOnExpire = function() return false end,
     GetAttributes = function(self) return MODIFIER_ATTRIBUTE_PERMANENT end,
     DeclareFunctions = function(self) return {
+        MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE,
         MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE,
-        MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE,
         MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
         MODIFIER_PROPERTY_BASEATTACK_BONUSDAMAGE,
         MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
         MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
         MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+        MODIFIER_PROPERTY_TOOLTIP,
     } end,
+    GetAbilityTextureName = function (self) return "unknown_amulet4" end,
+    OnTooltip = function(self) return self.def end,
     OnCreated = function (self,kv)
         self.ef_hp = 0
         self.def = 0
@@ -54,27 +57,6 @@ modifier_item_unknown_amulet_stats = class({
 		self.primary_attribute_pct = self:GetAbility():GetSpecialValueFor("charge_primary_attribute") * self:GetAbility():GetCurrentCharges()
         self.mode = self:GetAbility():GetSecondaryCharges()
     	self:GetParent():CalculateStatBonus(true)
-        --Strength Mode
-        if self:GetAbility():GetSecondaryCharges() == 1 then
-            self.ef_hp = (self:GetAbility():GetCurrentCharges() * self.all_damage_def_per_charge * self:GetParent():GetMaxHealth()) + self:GetParent():GetMaxHealth()
-            self.def = ((self:GetParent():GetMaxHealth() / self.ef_hp) - 1) * 100
-            print(self.def)
-        end
-        --Agility Mode
-        if self:GetAbility():GetSecondaryCharges() == 2 then
-            self.hand_damage = self:GetAbility():GetCurrentCharges() *  self.hand_damage_per_charge
-            print(self.hand_damage)
-        end
-        --Intellect Mode
-        if self:GetAbility():GetSecondaryCharges() == 3 then
-            self.spell_amp = self:GetAbility():GetCurrentCharges() * self.spell_amp_pre_charge
-            print(self.spell_amp)
-        end
-        --Univ Mode
-        if self:GetAbility():GetSecondaryCharges() == 4 then
-            self.base_damage = (self:GetParent():GetStrength() + self:GetParent():GetAgility() + self:GetParent():GetIntellect()) * self.stat_damage * self:GetAbility():GetCurrentCharges()
-            print(self.base_damage)
-        end
 	end,
 	GetModifierBonusStats_Strength = function (self)
 		if self.mode == 1 then
@@ -105,22 +87,27 @@ modifier_item_unknown_amulet_stats = class({
 	end,
     GetModifierIncomingDamage_Percentage = function (self)
         if self:GetAbility():GetSecondaryCharges() == 1 then
+            self.ef_hp = (self:GetAbility():GetCurrentCharges() * self.all_damage_def_per_charge * self:GetParent():GetMaxHealth()) + self:GetParent():GetMaxHealth()
+            self.def = ((self:GetParent():GetMaxHealth() / self.ef_hp) - 1) * 100
             return self.def
         end
     end,  
-    GetModifierDamageOutgoing_Percentage = function (self)
+    GetModifierTotalDamageOutgoing_Percentage = function(self,kv) 
         if self:GetAbility():GetSecondaryCharges() == 2 then
-            return self.hand_damage
-        end 
+        if kv.attacker ~= self:GetParent() then return end
+        if kv.damage_type ~= DAMAGE_TYPE_PHYSICAL then return end
+        return self:GetAbility():GetCurrentCharges() *  self.hand_damage_per_charge
+        end
     end,
     GetModifierSpellAmplify_Percentage = function (self)
         if self:GetAbility():GetSecondaryCharges() == 3 then
-            return self.spell_amp 
+            return self:GetAbility():GetCurrentCharges() * self.spell_amp_pre_charge
          end 
     end,
     GetModifierBaseAttack_BonusDamage = function (self)
         if self:GetAbility():GetSecondaryCharges() == 4 then
-            return self.base_damage
+            return (self:GetParent():GetStrength() + self:GetParent():GetAgility() + self:GetParent():GetIntellect()) * self.stat_damage * self:GetAbility():GetCurrentCharges()
         end
     end,
+    
 })
